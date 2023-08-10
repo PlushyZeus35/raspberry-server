7ee49d2f08da15d52aa52ebfe969bbd44b3d5519
Borja Lorenzo <plushyzeus35@gmail.com>
2023-08-10 15:24:50 +0200

    updated script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,12 @@
+# Stop service
 service plushyserver stop
+# Delete temporal and frontend files
 rm -r /server/raspberry-server/node_modules
-cd /server/raspberry-server && npm i
 rm -r /var/www/html/server/
+# Update codebase and install dependencies
 cd /server/raspberry-server && git pull
+cd /server/raspberry-server && npm i
+# Set new frontend pages
 cp -r /server/raspberry-server/frontend /var/www/html/server
+# Restart service
 service plushyserver restart
